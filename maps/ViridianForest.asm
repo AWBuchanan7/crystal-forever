bce293a13380cd9b83e1d041fe972e6881f02eee
Aubrey Buchanan <buchananaubrey7@gmail.com>
2019-03-26 20:29:02 -0600

    Bugfixes, add Scope Lens to Viridian Forest

--- maps/ViridianForest.asm
+++ maps/ViridianForest.asm
@@ -1,6 +1,9 @@
 	const_def 2 ; object constants
 	const VIRIDIAN_FOREST
 
+ViridianForestScopeLens:
+	itemball SCOPE_LENS
+
 ViridianForest_MapScripts:
 	db 0 ; scene scripts
 
@@ -9,12 +12,14 @@ ViridianForest_MapScripts:
 ViridianForest_MapEvents:
 	db 0, 0 ; filler
 
-	db 2 ; warp events
-	warp_event 1, 0, ROUTE_2, 4
-	warp_event 16, 47, ROUTE_2, 5
+	db 3 ; warp events
+	warp_event 1, 0, ROUTE_2, 6
+	warp_event 16, 47, ROUTE_2, 7
+	warp_event 17, 47, ROUTE_2, 7
 	
 	db 0 ; coord events
 
 	db 0 ; bg events
 
-	db 0 ; object events
+	db 1 ; object events
+	object_event  1, 33, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, ViridianForestScopeLens, EVENT_VIRIDIAN_FOREST_SCOPE_LENS
